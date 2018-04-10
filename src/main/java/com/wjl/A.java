0a188371f29f256eaf78ffc29b6ebc960c705c01
wangjl <wangjialing@hmtime.com>
2018-04-10 12:00:08 +0800

    0

---
+++ src/main/java/com/wjl/A.java
@@ -0,0 +1,10 @@
+package com.wjl;
+
+/**
+ * Created by jialing.wang on 2018/04/10.
+ */
+public class A {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/wjl/B.java
@@ -0,0 +1,10 @@
+package com.wjl;
+
+/**
+ * Created by jialing.wang on 2018/04/10.
+ */
+public class B {
+    public static void main(String[] args) {
+
+    }
+}

97c8a7e73dc75682f49ff3bcd9adff29bcc31eaa
wangjl <wangjialing@hmtime.com>
2018-04-10 12:01:24 +0800

    2

--- src/main/java/com/wjl/A.java
+++ src/main/java/com/wjl/A.java
@@ -6,5 +6,6 @@ package com.wjl;
 public class A {
     public static void main(String[] args) {
         int a=1;
+        int b=2;
     }
 }
--- src/main/java/com/wjl/B.java
+++ src/main/java/com/wjl/B.java
@@ -6,5 +6,6 @@ package com.wjl;
 public class B {
     public static void main(String[] args) {
         int a=1;
+        int b=2;
     }
 }
